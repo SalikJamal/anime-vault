65fecc7cb7bd2a58028ea8303046503bb4145bb3
Salik <iamsalikjamal@gmail.com>
2024-01-18 15:54:44 +0500

    Data fetching with server actions

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,9 @@ import Image from "next/image"
 export default function Footer() {
   return (
     <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
-      <p className="text-base font-bold text-white">@salikjamal20</p>
+      <a className="text-base font-bold text-white" href='https://twitter.com/salikjamal20' target="_blank">
+        @salikjamal20
+      </a>
       <Image
         src="./logo.svg"
         alt="logo"
